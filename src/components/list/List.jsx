31e8d58051909d73bdbb3fc96f2d54ad1ea4a3e5
ertugakmann <134059022+ertugakmann@users.noreply.github.com>
2024-07-08 10:27:49 +0300

    Update HTML title and fix CSS styling in ChatList and UserInfo components

--- src/components/list/List.jsx
+++ src/components/list/List.jsx
@@ -1,8 +1,15 @@
 import React from "react";
 import "../list/list.css";
+import UserInfo from "./UserInfo/UserInfo";
+import ChatList from "./ChatList/ChatList";
 
 function List() {
-  return <div className="list">List</div>;
+  return (
+    <div className="list">
+      <UserInfo />
+      <ChatList />
+    </div>
+  );
 }
 
 export default List;
