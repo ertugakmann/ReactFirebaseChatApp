8cb7c3059583260b3d51137afb8f8d4d5b581aec
ertugakmann <134059022+ertugakmann@users.noreply.github.com>
2024-07-09 14:55:11 +0300

    Detail section added

--- src/components/detail/Detail.jsx
+++ src/components/detail/Detail.jsx
@@ -2,7 +2,84 @@ import React from "react";
 import "./detail.css";
 
 function Detail() {
-  return <div className="detail">Detail</div>;
+  return (
+    <div className="detail">
+      <div className="user">
+        <img src="./avatar.png" alt="" />
+        <h2>John Doe</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
+      </div>
+      <div className="info">
+        <div className="option">
+          <div className="title">
+            <span>Chat Settings</span>
+            <img src="./arrowUp.png" alt="" />
+          </div>
+        </div>
+        <div className="option">
+          <div className="title">
+            <span>Privacy & Help</span>
+            <img src="./arrowUp.png" alt="" />
+          </div>
+        </div>
+        <div className="option">
+          <div className="title">
+            <span>Shared Photos</span>
+            <img src="./arrowDown.png" alt="" />
+          </div>
+        </div>
+        <div className="photos">
+          <div className="photoItem">
+            <div className="photoDetail">
+              <img
+                src="https://images.pexels.com/photos/10515653/pexels-photo-10515653.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+                alt=""
+              />
+              <span>photo_2024_2.png</span>
+            </div>
+            <img src="./download.png" className="icon" alt="" />
+          </div>
+          <div className="photoItem">
+            <div className="photoDetail">
+              <img
+                src="https://images.pexels.com/photos/10515653/pexels-photo-10515653.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+                alt=""
+              />
+              <span>photo_2024_2.png</span>
+            </div>
+            <img src="./download.png" className="icon" alt="" />
+          </div>
+          <div className="photoItem">
+            <div className="photoDetail">
+              <img
+                src="https://images.pexels.com/photos/10515653/pexels-photo-10515653.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+                alt=""
+              />
+              <span>photo_2024_2.png</span>
+            </div>
+            <img src="./download.png" className="icon" alt="" />
+          </div>
+          <div className="photoItem">
+            <div className="photoDetail">
+              <img
+                src="https://images.pexels.com/photos/10515653/pexels-photo-10515653.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+                alt=""
+              />
+              <span>photo_2024_2.png</span>
+            </div>
+            <img src="./download.png" className="icon" alt="" />
+          </div>
+        </div>
+        <div className="option">
+          <div className="title">
+            <span>Shared Files</span>
+            <img src="./arrowDown.png" alt="" />
+          </div>
+        </div>
+        <button>Block User</button>
+      </div>
+    </div>
+  );
 }
 
 export default Detail;
